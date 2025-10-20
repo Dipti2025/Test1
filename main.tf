5a020a20f896a5ca8359d11742714608199cde67
Dipti <diptisable2025@gmailc.om>
2025-10-21 04:13:12 +0530

    added vpc

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1" # Replace with your desired AWS region
+}
+
+# Define the VPC resource
+resource "aws_vpc" "main_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "my-terraform-vpc"
+    Environment = "development"
+  }
+}
