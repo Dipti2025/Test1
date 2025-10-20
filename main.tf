973c0ffffde4f9c7421a6a5130afcb12a091f538
Dipti <diptisable2025@gmailc.om>
2025-10-21 04:20:14 +0530

    new vpc added

--- main.tf
+++ main.tf
@@ -14,4 +14,18 @@ resource "aws_vpc" "main_vpc" {
     Name        = "my-terraform-vpc"
     Environment = "development"
   }
+  }
+# Define the VPC resource
+resource "aws_vpc" "main_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "my-terraform-vpc"
+    Environment = "development"
+  }
+}
 }
